03909aab3aa9acc8e5fa2046d99411a913b87b6e
Rohan Pansara <rohan.pansara@drcsystems.com>
2024-07-22 17:20:53 +0530

    Role Based Authentication Checked

---
+++ src/main/java/com/jwtauthentication/utils/CustomDateTimeFormatter.java
@@ -0,0 +1,13 @@
+package com.jwtauthentication.utils;
+
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+public class DateTimeFormater {
+    public static String localDateTimeToString(String localDateTime){
+        DateTimeFormater dateTimeFormater = DateTimeFormater.of
+    }
+
+}
