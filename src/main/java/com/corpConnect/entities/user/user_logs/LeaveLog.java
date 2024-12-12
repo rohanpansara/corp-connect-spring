631f8b9af8d28c8d929b5d933afab321bade5b77
Rohan Pansara <rohanpansara2632@gmail.com>
2024-12-12 21:48:04 +0530

    Renamed logs to user_logs

---
+++ src/main/java/com/corpConnect/entities/user/user_logs/LeaveLog.java
@@ -0,0 +1,27 @@
+package com.corpConnect.entities.user.user_logs;
+
+import com.corpConnect.entities.common.BaseEntity;
+import com.corpConnect.entities.user.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Table(name = "USER_LOGS_LEAVE")
+public class LeaveLog extends BaseEntity {
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+}
