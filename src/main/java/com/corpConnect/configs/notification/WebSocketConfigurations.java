c1d8fb198e65630508b4080cd1c52602c4ba0052
Rohan Pansara <rohanpansara2632@gmail.com>
2025-01-04 19:14:53 +0530

    Added dependencies for web socket

---
+++ src/main/java/com/corpConnect/configs/notification/WebSocketConfigurations.java
@@ -0,0 +1,25 @@
+package com.corpConnect.configs.notification;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfigurations implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic", "/queue"); // Add /queue for user-specific notifications
+        config.setApplicationDestinationPrefixes("/app");
+        config.setUserDestinationPrefix("/user"); // Allows sending messages to specific users
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS(); // Use SockJS for fallback support
+    }
+
+}
